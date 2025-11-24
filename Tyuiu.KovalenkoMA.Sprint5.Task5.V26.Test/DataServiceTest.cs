73e30fc7d9a95e695314d13d2d4548e2e8d6de77
Margo Kovalenko <Tyuiu.KovalenkoM@outlook.com>
2025-11-25 01:03:22 +0500

    task5

---
+++ Tyuiu.KovalenkoMA.Sprint5.Task5.V26.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.KovalenkoMA.Sprint5.Task5.V26.Lib;
+namespace Tyuiu.KovalenkoMA.Sprint5.Task5.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = $@"C:\DataSprint5\InPutDataFileTask5V26.txt";
+            FileInfo fileinto = new FileInfo(path);
+            bool fileExists = fileinto.Exists;
+            Assert.AreEqual(true, fileExists);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string path = $@"C:\DataSprint5\InPutDataFileTask5V26.txt";
+            FileInfo fileinto = new FileInfo(path);
+            bool fileExists = fileinto.Exists;
+            Assert.AreEqual(true, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.KovalenkoMA.Sprint5.Task5.V26/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.KovalenkoMA.Sprint5.Task5.V26.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #5 | Выполнила: Коваленко М. А. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #26                                                             *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        DataService ds = new DataService();
+        string path = $@"C:\DataSprint5\InPutDataFileTask5V26.txt";
+        Console.WriteLine($"* Файл сохранен: {path}                                                  *");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+
+        Console.WriteLine($"* Результат: {ds.LoadFromDataFile(path)}                                 *");
+
+
+        Console.ReadKey();
+    }
+}
